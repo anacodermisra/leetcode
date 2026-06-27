3f806ca6504f4d7c61c8b80161ad567a02fccc6d
anacodermisra <anandmisraaa@gmail.com>
2026-06-27 19:03:32 +0530

    Time: 0 ms (100%), Space: 20.7 MB (93.7%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -30,12 +30,12 @@ public:
             }
         }
         for(int j=1; j<n;j++){
-            if(matrix[0][0]==0 || matrix[0][j]==0){
+            if(matrix[0][0]==0){
                 matrix[0][j]=0;
             }
         }
         for(int i=0; i<m;i++){
-            if(col0==0 || matrix[i][0]==0){
+            if(col0==0){
                 matrix[i][0]=0;
             }
         }
