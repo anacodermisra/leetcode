46494e2483164fac6097ccfefe1cdb3c438516bf
anacodermisra <anandmisraaa@gmail.com>
2026-07-03 23:30:51 +0530

    Time: 0 ms (100%), Space: 20.7 MB (58.34%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,16 +1,10 @@
-
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int single =0;
-        if(nums.size()==1){
-            return nums[0];
+        int xxor=0;
+        for(int i=0;i<nums.size();i++){
+            xxor= xxor ^ nums[i];
         }
-        sort(nums.begin(),nums.end());
-       for (int i = 0; i < nums.size() - 1; i += 2) {
-         if (nums[i] != nums[i + 1])
-         return nums[i];
-}
-return nums.back();
+        return xxor;
     }
 };
