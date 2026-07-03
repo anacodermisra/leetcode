6777841553c2bb9c8c548a3411bce8d1b50bafd7
anacodermisra <anandmisraaa@gmail.com>
2026-07-03 23:29:24 +0530

    Time: 3 ms (22.96%), Space: 20.6 MB (83.55%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -7,14 +7,10 @@ public:
             return nums[0];
         }
         sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==nums[i+1]){
-                i++;
-            }
-            else{
-                single= nums[i];
-            }
-        }
-        return single;
+       for (int i = 0; i < nums.size() - 1; i += 2) {
+         if (nums[i] != nums[i + 1])
+         return nums[i];
+}
+return nums.back();
     }
 };
