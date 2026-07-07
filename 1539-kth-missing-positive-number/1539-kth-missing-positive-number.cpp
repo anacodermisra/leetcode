f8083663a57d50aef576c0de21b05e24dbbc6afb
anacodermisra <anandmisraaa@gmail.com>
2026-07-08 02:01:33 +0530

    Time: 0 ms (100%), Space: 13.3 MB (29.25%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -13,12 +13,17 @@ public:
             int missing=missingnos(arr,k,mid);
             if(missing<k){
                 low=mid+1;
-                sol=missing;
+               
             }
             else{
                 high=mid-1;
             }  
         }
+        //return arr[high]+(k-missing[high])
+       // arr[high]+k -(arr[high]-(high+1))
+       //arr[high]+k-arr[high]+high+1
+       //k+high+1
+       //k+low
         return low+k;
     }
 };
