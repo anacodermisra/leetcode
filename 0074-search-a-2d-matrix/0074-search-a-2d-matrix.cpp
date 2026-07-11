facab695aa724f0c6d34fefc5defd587dd4381ed
anacodermisra <anandmisraaa@gmail.com>
2026-07-11 21:55:54 +0530

    Time: 0 ms (100%), Space: 13.2 MB (79.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,13 +3,18 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row=matrix.size();
         int col=matrix[0].size();
-        int i=0;
-        int j=col-1;
-        while(i<row && j>=0){
-            if(target==matrix[i][j]) return true;
+        int low =0;
+        int high=row*col-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int rowindex=mid/col;
+            int colindex=mid%col;
+            if(target==matrix[rowindex][colindex]) return true;
 
-            else if(target>matrix[i][j]) i++;
-            else if (target<matrix[i][j]) j--;
+            else if(target>matrix[rowindex][colindex])low=mid+1 ;
+
+            else if(target<matrix[rowindex][colindex])high=mid-1 ;
+            
         }
         return false;
     }
